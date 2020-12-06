340ea5a0e13b075e5270f2b9e83537d7f411f273
Max Garanin <maxgaranin@gmail.com>
2020-12-06 13:58:40 +0400

    feat: use localhost cart-api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://vgu8neei3m.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://tdsss8rkpd.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://vgu8neei3m.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://localhost:4000/api',
 };
 
 export default API_PATHS;
