77d6b9d4097d0215e926c6996bfd1f427c91403f
Max Garanin <maxgaranin@gmail.com>
2020-12-06 15:00:52 +0400

    feat: use elastic-beanstalk api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://vgu8neei3m.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://tdsss8rkpd.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://vgu8neei3m.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://localhost:4000/api',
+  cart: 'http://maxgaranin-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
